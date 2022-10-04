c983513adc5dec022b74f1bf6457fb3a2fd4e502
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-04 15:20:43 +0900

    課題5

--- ex02/culc.py
+++ ex02/culc.py
@@ -5,7 +5,7 @@ def click_number(event):
     btn = event.widget
     num = int(btn["text"])
     tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
-
+    entry.insert(tk.END, num)
 
 root = tk.Tk()
 root.geometry("300x500")
