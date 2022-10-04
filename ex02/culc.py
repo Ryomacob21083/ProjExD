acd87288e675fa68315ecb20b98d18f3c15a16a1
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-04 15:06:42 +0900

    課題2

--- ex02/culc.py
+++ ex02/culc.py
@@ -3,4 +3,15 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("300x500")
 
+c = 0
+r = 0
+for i, num in enumerate(range(9, -1, -1), 1):
+    btn = tk.Button(root, text = f"{num}", font = ("", 30), width = 4, height = 2)
+    btn.grid(row = r, column = c)
+    c += 1
+
+    if i%3 == 0:
+        r += 1
+        c = 0
+
 root.mainloop()
