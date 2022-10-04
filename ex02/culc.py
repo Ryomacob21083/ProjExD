2c499ba368a574f645558ba231547abf2f76879a
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-04 15:23:44 +0900

    課題3

--- ex02/culc.py
+++ ex02/culc.py
@@ -5,16 +5,12 @@ def click_number(event):
     btn = event.widget
     num = int(btn["text"])
     tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
-    entry.insert(tk.END, num)
 
 root = tk.Tk()
 root.geometry("300x500")
 
-entry = tk.Entry(root, width = 10, font = (", 40"), justify = "right")
-entry.grid(row = 0, column = 0, columnspan = 3)
-
 c = 0
-r = 1
+r = 0
 for i, num in enumerate(range(9, -1, -1), 1):
     btn = tk.Button(root, text = f"{num}", font = ("", 30), width = 4, height = 2)
     btn.bind("<1>", click_number)
