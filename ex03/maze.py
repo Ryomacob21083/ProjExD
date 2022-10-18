0438ae525b539beefa10f161b9b81fbadcd331bb
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-18 14:11:32 +0900

    練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,4 +3,5 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷える工科とん")
+    canv = tk.Canvas(root, width=1500, height=900, background="black")
     root.mainloop()
