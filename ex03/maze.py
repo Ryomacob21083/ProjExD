baf3cdb2df2f813e88b07903775672945c5242e3
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-18 14:20:12 +0900

    練習3

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,7 +2,11 @@ import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷える工科とん")
+    root.title("迷えるこうかとん")
     canv = tk.Canvas(root, width=1500, height=900, background="black")
     canv.pack()
+
+    tori = tk.PhotoImage(file="ex03/fig/3.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="こうかとん")
     root.mainloop()
