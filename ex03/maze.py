17d1e2a24b65b59d6320f29ba5d6493e4d16e53c
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-18 14:12:52 +0900

    練習2改

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷える工科とん")
     canv = tk.Canvas(root, width=1500, height=900, background="black")
+    canv.pack()
     root.mainloop()
