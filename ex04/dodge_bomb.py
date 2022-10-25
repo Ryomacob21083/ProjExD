85ebfb3820ffb6bf22a3e1382e7f1826d43e4632
shimozato <c0b2108363@edu.teu.ac.jp>
2022-10-25 16:50:14 +0900

    爆弾の加速、失敗メッセージ

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,8 +1,12 @@
 import pygame as pg
 import sys
 from random import randint
+import tkinter as tk
+import tkinter.messagebox as tkm
 
-def check_bound(obj_rct, scr_rct): #練習７
+life = 3
+
+def check_bound(obj_rct, scr_rct):
     #obj_rct:こうかとんrectまたは爆弾rect
     #scr_rct:スクリーンrect
 
@@ -11,50 +15,50 @@ def check_bound(obj_rct, scr_rct): #練習７
         yoko = -1
     if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
         tate = -1
+
     return yoko, tate
 
 
-def main(): #練習１
+def main(): 
+    global life
+    root = tk.Tk()
+    root.withdraw()
     pg.display.set_caption("逃げろ！こうかとん")    ##タイトルバーに「初めての...」を表示
     scrn_sfc = pg.display.set_mode((1600, 900)) ## 800×600の画面surfaceを生成
     scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
-    #練習３
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
-    #練習5
     bomb_sfc = pg.Surface((20, 20))
     bomb_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = randint(0, scrn_rct.width)
     bomb_rct.centery = randint(0, scrn_rct.height)
-    #練習6
+
     vx, vy = +1, +1
-
-
     clock = pg.time.Clock()
-    while True: #練習２
+    while True: 
         scrn_sfc.blit(bg_sfc, bg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         
-        key_states = pg.key.get_pressed() #練習4
+        key_states = pg.key.get_pressed() 
         if key_states[pg.K_UP]:
-            tori_rct.centery -= 1
+            tori_rct.centery -= 5
         if key_states[pg.K_DOWN]:
-            tori_rct.centery += 1
+            tori_rct.centery += 5
         if key_states[pg.K_LEFT]:
-            tori_rct.centerx -= 1
+            tori_rct.centerx -= 5
         if key_states[pg.K_RIGHT]:
-            tori_rct.centerx += 1
-        yoko, tate = check_bound(tori_rct, scrn_rct)   #練習7
+            tori_rct.centerx += 5
+        yoko, tate = check_bound(tori_rct, scrn_rct)  
         if yoko == -1:
             if key_states[pg.K_LEFT]:
                 tori_rct.centerx += 1
@@ -66,16 +70,29 @@ def main(): #練習１
             if key_states[pg.K_DOWN]:
                 tori_rct.centery -= 1
 
-        scrn_sfc.blit(tori_sfc, tori_rct) #練習３
-        yoko, tate = check_bound(bomb_rct, scrn_rct)#練習7
+        scrn_sfc.blit(tori_sfc, tori_rct) 
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
         vx *= yoko
         vy *= tate
-        bomb_rct.move_ip(vx, vy)          #練習６
-        scrn_sfc.blit(bomb_sfc, bomb_rct) #練習５
 
-        #練習８
+        if vx < 0:
+            vx -= 0.001
+        else:
+            vx += 0.001
+        if vy < 0:
+            vy -= 0.001
+        else:
+            vy += 0.001
+
+        bomb_rct.move_ip(vx, vy)          
+        scrn_sfc.blit(bomb_sfc, bomb_rct) 
+
         if tori_rct.colliderect(bomb_rct):
-            return
+            life -= 1                
+            if life == 0:
+                tkm.showinfo("GANE OVER", "また挑戦してね")
+                return
+
         pg.display.update()
         clock.tick(1000)
     
